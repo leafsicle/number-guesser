f120f88c93cec9e848df6233005a3f53270135fd
Matt Cooke <cookem529@gmail.com>
2018-10-22 17:02:57 -0400

    complete. I <3 Ruby

--- guess.rb
+++ guess.rb
@@ -1,13 +1,34 @@
-Min=0
-Max=1000
-lower = 0
-upper = 1000
+min=0
+max=1000
+guess= 500 #this is the middle of the range of #'s'
 counter=0
 
-puts "Pick a number between 0 and 1000; is your number between 500 and 1000?"
-While true do
-  choice = gets.chomp
-  
-  puts "bacon"
+puts "Pick a number between 0 and 1000"
+puts "is your number between 501 and 1000?"
+puts " Type\'y\' for yes, \'h\' for higher number or \'l\' for min number\n"
 
+while true
+  choice = gets.chomp
+
+  if choice == "y"
+    puts "I did it in #{counter} turns!"
+    break
+  end
+
+  if choice == "h"
+    min=guess+1
+    guess= ((max-min)/2)+min
+
+    puts "Is your answer #{guess}?"
+    counter+=1
+  end
+
+  if choice == "l"
+    max=guess-1
+    guess= ((min-max)/2)+max
+    puts "Is your answer #{guess}?"
+    counter+=1
+  end
 end
+
+
