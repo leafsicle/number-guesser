3773cdf8cf4e75e81abd709cc9ac13f52841715c
Matt Cooke <cookem529@gmail.com>
2018-10-22 15:41:34 -0400

    Initial commit

---
+++ guess.rb
@@ -0,0 +1,13 @@
+Min=0
+Max=1000
+lower = 0
+upper = 1000
+counter=0
+
+puts "Pick a number between 0 and 1000; is your number between 500 and 1000?"
+While true do
+  choice = gets.chomp
+  
+  puts "bacon"
+
+end
