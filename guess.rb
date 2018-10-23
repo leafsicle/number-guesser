200a4546bd5e643a06dc5ab7553282e2b94ba36e
Matt Cooke <cookem529@gmail.com>
2018-10-23 08:47:06 -0400

    added a timeout feature

--- guess.rb
+++ guess.rb
@@ -1,33 +1,48 @@
-min=0
-max=1000
-guess= 500 #this is the middle of the range of #'s'
-counter=0
+min=0 #this is the lowest possible value for the data set
+max=1000 # this is the maximum value for our data set currently. It can increase limitlessly
+guess= (max+min)/2 #this is the middle of the range of #'s'
+counter=0 #this should start at 0. Clearly
+correct = "Type\'y\' for yes\n"
+higher ="Type \'h\' if your number is higher\n"
+lower = "Type \'l\' if your number is lower\n"
 
-puts "Pick a number between 0 and 1000"
-puts "is your number between 501 and 1000?"
-puts " Type\'y\' for yes, \'h\' for higher number or \'l\' for min number\n"
+puts "Think of a number between #{min} to #{max}. Got it?\n"
+puts "Type \'h\' if your number is from #{max+((min-max)/2)}-#{max}\n"
+puts " Tyoe \'l\' if your number is from #{min}-#{min+((max-min)/2)}\n"
 
 while true
   choice = gets.chomp
 
-  if choice == "y"
+  #The next lines dictate that if the choice is 'y' for yes then the game 
+  #is over and the terminal will return the number of turns used to guess the correct number
+  if choice == "y"|| choice =="Y"
     puts "I did it in #{counter} turns!"
+    sleep 3
+    system('clear')
     break
   end
 
-  if choice == "h"
+  #The 'H' section says that the player's number is higher and therefore the cmoputer
+  #will take the higher end of the range of numbers and will bisect the choices remaining 
+  #from the last range of values
+  if choice == "h"|| choice =="H"
+    correct
     min=guess+1
-    guess= ((max-min)/2)+min
-
-    puts "Is your answer #{guess}?"
+    guess= min+((max-min)/2)
+    puts "Is your answer #{guess}?  #{correct} #{higher} #{lower}"
     counter+=1
   end
 
-  if choice == "l"
+  if choice == "l"|| choice =="L"
+    correct
     max=guess-1
-    guess= ((min-max)/2)+max
-    puts "Is your answer #{guess}?"
+    guess= max+((min-max)/2)
+    puts "Is your answer #{guess}? #{correct} #{higher} #{lower}"
     counter+=1
+  end
+
+  if guess<0 
+    puts "I think you have forgotten your number. Try typing CNTRL+C and try this again!"
   end
 end
 
